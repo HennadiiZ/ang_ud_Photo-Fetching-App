418da534bf1e26be4879c71f1998a3b687e82304
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-06-14 11:53:32 +0300

    creating a new component + service

---
+++ src/app/photo-show/photo-show.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-show',
+  templateUrl: './photo-show.component.html',
+  styleUrls: ['./photo-show.component.scss']
+})
+export class PhotoShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // https://unsplash.com/
+  // https://unsplash.com/developers
+  
+  // --------// Section 17: Exercise! A Photo-Fetching App
+  // 212. App Overview + 213. App Architecture Design + 214. API Signup
+  //
+}
